f2e32f9672425f780ea520e6a76cf62f5ad5d174
Crekto <hiimcrekto@yahoo.com>
2022-05-20 11:25:23 +0300

    Added compulsory

---
+++ Lab11/src/test/java/com/Crekto/Lab11/Lab11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Crekto.Lab11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
